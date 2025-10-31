34c4f8719326b379902cc16c5b509a9ec30efea8
hashir <muhammadhashir5889@gmail.com>
2025-10-31 22:00:28 +0500

    50% complete.

--- dist/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const btn = document.getElementById("btn");
-if (btn) {
-    btn.addEventListener("click", () => {
-        alert("Hello");
-    });
-}
-//# sourceMappingURL=script.js.map

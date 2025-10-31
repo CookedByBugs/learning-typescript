15819dc4a98dea3f4b8e64cc3db55542eba11a4e
hashir <muhammadhashir5889@gmail.com>
2025-10-31 23:35:28 +0500

    Learned functions

--- Assignment 1/dist/script.d.ts
+++ Assignment 1/dist/script.d.ts
@@ -2,4 +2,8 @@ declare const setYear: HTMLElement | null;
 declare const alertName: () => void;
 declare const alertNumber: () => void;
 declare const variableName: () => void;
+declare const camelCase: () => void;
+declare const sum: () => void;
+declare const clearStatement: () => void;
+declare const clearOutput: () => void;
 //# sourceMappingURL=script.d.ts.map
--- Assignment 1/dist/script.js
+++ Assignment 1/dist/script.js
@@ -3,6 +3,8 @@ const setYear = document.getElementById("year");
 if (setYear)
     setYear.innerHTML = new Date().getFullYear().toString();
 const alertName = () => {
+    clearOutput();
+    clearStatement();
     const statement = document.getElementById("statement");
     if (statement)
         statement.innerHTML = '<p class="text-center">alert(name);</p>';
@@ -10,6 +12,8 @@ const alertName = () => {
     alert(name);
 };
 const alertNumber = () => {
+    clearOutput();
+    clearStatement();
     const statement = document.getElementById("statement");
     if (statement)
         statement.innerHTML = '<p class="text-center">alert(24408);</p>';
@@ -17,10 +21,49 @@ const alertNumber = () => {
     alert(num);
 };
 const variableName = () => {
+    clearOutput();
+    clearStatement();
     const html = "<ul class='list-disc'><li>A variable name can't contain any spaces.</li><li>A variable name can contain only letters, numbers, dollar signs, and underscores.</li><li>Though a variable name can't be any of JavaScript's keywords, it can contain keywords.</li><li>For example, <code>userAlert</code> and <code>myVar</code> are legal.</li><li>Capital letters are fine, but be careful. Variable names are case sensitive. A <code>rose</code> is not a <code>Rose</code>. If you assign the string \"Floribundas\" to the variable <code>rose</code>, and then ask JavaScript for the value assigned to <code>Rose</code>, you'll come up empty.</li><li>I teach the camelCase naming convention. Why \"camelCase\"? Because there is a hump or two (or three) in the middle if the name is formed by more than one word. A camelCase name begins in lower case. If there's more than one word in the name, each subsequent word gets an initial cap, creating a hump. If you form a variable name with only one word, like <code>response</code>, there's no hump. It's a camel that's out of food. Please adopt the camelCase convention. It'll make your variable names more readable, and you'll be less likely to get variable names mixed up.</li></ul>";
     const element = document.getElementById("output");
     if (element) {
         element.innerHTML = html;
     }
+};
+const camelCase = () => {
+    clearOutput();
+    clearStatement();
+    const html = "<p class='text-center'><code>camelCase <br> addTwoNumber <br> subtractTwoNumber</code></p>";
+    const statement = document.getElementById("statement");
+    const output = document.getElementById("output");
+    if (statement)
+        statement.innerHTML =
+            '<p class="text-center">if (statement) statement.innerHTML = "html";</p>';
+    if (output)
+        output.innerHTML = html;
+};
+const sum = () => {
+    const num1 = prompt("Enter first number");
+    const n1 = Number(num1);
+    const num2 = prompt("Enter second number");
+    const n2 = Number(num2);
+    console.log(n1, n2);
+    const sum = n1 + n2;
+    const statement = document.getElementById("statement");
+    const output = document.getElementById("output");
+    if (statement)
+        statement.innerHTML =
+            "<p class='text-center'>if(output) output.innerHTML = sum.toString()</p>";
+    if (output)
+        output.innerHTML = "<p class='text-center'>" + sum.toString() + "</p>";
+};
+const clearStatement = () => {
+    const statement = document.getElementById("statement");
+    if (statement)
+        statement.innerHTML = "";
+};
+const clearOutput = () => {
+    const output = document.getElementById("output");
+    if (output)
+        output.innerHTML = "";
 };
 //# sourceMappingURL=script.js.map
